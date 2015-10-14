c100e19efb26a0746c8a01820dc697160d8db1c7
Daniel Mason <daniel.mason@thefoundry.co.uk>
2015-10-14 16:42:09 +0100

    Help find classes by name (needs improving)

---
+++ src/Helper/ClassRegistry.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * ClassRegistry.php
+ *
+ * @author    Daniel Mason <daniel.mason@thefoundry.co.uk>
+ * @copyright 2015 The Foundry Visionmongers
+ * @license
+ * @see       https://github.com/Visionmongers/
+ */
+
+
+namespace Foundry\Masonry\Builder\Helper;
+
+/**
+ * Class ClassRegistry
+ *
+ * @package Masonry-Builder
+ * @see     https://github.com/Visionmongers/
+ */
+class ClassRegistry
+{
+
+    protected $classNames = [];
+
+    public function __construct(array $fullyQualifiedNames = [])
+    {
+        $this->addClassNames($fullyQualifiedNames);
+    }
+
+    /**
+     * Add a group of class names to the registry
+     * @param array $fullyQualifiedNames
+     * @return $this
+     */
+    public function addClassNames(array $fullyQualifiedNames = [])
+    {
+        foreach($fullyQualifiedNames as $fullyQualifiedName) {
+            if(!is_string($fullyQualifiedName)) {
+                throw new \InvalidArgumentException('Class names must be strings');
+            }
+            $this->addClassName($fullyQualifiedName);
+        }
+        return $this;
+    }
+
+    /**
+     * Add a class name to the registry
+     * @param $fullyQualifiedName
+     * @return $this
+     */
+    public function addClassName($fullyQualifiedName)
+    {
+        $this->classNames[$fullyQualifiedName] = $fullyQualifiedName;
+        return $this;
+    }
+
+    /**
+     * Get a fully qualified class name from a partial class name
+     * @param $name
+     * @return string
+     */
+    public function getClass($name)
+    {
+        $fullyQualifiedName = $this->classNameLookup($name);
+        if(class_exists($fullyQualifiedName)) {
+            return $fullyQualifiedName;
+        }
+        throw new \UnexpectedValueException("Class '$fullyQualifiedName' was registered but could not be found");
+    }
+
+    /**
+     * Find a class in the list of class names
+     * @param $name
+     * @throws \UnexpectedValueException
+     * @return string
+     */
+    protected function classNameLookup($name)
+    {
+        // Option 1, the class name is already correct
+        if(array_key_exists($name, $this->classNames)) {
+            return $name;
+        }
+        // Option 2, the class name is a partial match at the end
+        foreach($this->classNames as $fullyQualifiedName) {
+            if(preg_match("/{$name}$/", $fullyQualifiedName)) {
+                return $fullyQualifiedName;
+            }
+        }
+        // Option 3, the class name is a partial match... somewhere
+        foreach($this->classNames as $fullyQualifiedName) {
+            if(preg_match("/{$name}/", $fullyQualifiedName)) {
+                return $fullyQualifiedName;
+            }
+        }
+        throw new \UnexpectedValueException("Could not find class matching '{$name}'");
+    }
+
+}
