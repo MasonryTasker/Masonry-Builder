d7660e93ece5c3f630fef5982ecf080424af6182
Daniel Mason <daniel.mason@thefoundry.co.uk>
2015-10-16 15:24:29 +0100

    Improve exec to prevent all output

--- src/Helper/System.php
+++ src/Helper/System.php
@@ -25,13 +25,39 @@ class System
      * Except now it works like someone with a brain would have made it, returning the exit code. You can still
      * optionally pass in an array to capture output and if you need the last line, use end() on the array.
      * @param string $command The command to run
-     * @param array $output A reference to an array that will hold the output from the exec
+     * @param string $stdOutput A reference to an array that will hold the output from the exec
+     * @param string $stdError  A reference to an array that will hold the output from the exec
+     * @throws \RuntimeException
      * @return int The exit code
      */
-    public function exec($command, array &$output = [])
-    {
-        $returnVar = null;
-        exec($command, $output, $returnVar);
-        return $returnVar;
-    }
+        public function exec($command, &$stdOutput = '', &$stdError = '')
+        {
+            $process = proc_open(
+                $command,
+                [
+                    0 => ['pipe', 'r'],
+                    1 => ['pipe', 'w'],
+                    2 => ['pipe', 'w'],
+                ],
+                $pipes
+            );
+
+            if (!is_resource($process)) {
+                throw new \RuntimeException('Could not create a valid process');
+            }
+
+            // This will prevent to program from continuing until the processes is complete
+            // Note: status code is created by this
+            $status = proc_get_status($process);
+            while($status['running']) {
+                $status = proc_get_status($process);
+            }
+
+            proc_close($process);
+
+            $stdOutput = stream_get_contents($pipes[1]);
+            $stdError  = stream_get_contents($pipes[2]);
+
+            return $status['exitcode'];
+        }
 }
